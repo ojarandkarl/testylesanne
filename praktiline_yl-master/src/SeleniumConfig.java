34fd8a334681f4662d08b7f37d259d53107f4558
Karl O <103935487+ojarandkarl@users.noreply.github.com>
2023-05-26 14:58:32 +0300

    SeleniumConfig.java
    Kalkulaatori test

--- praktiline_yl-master/src/SeleniumConfig.java
+++ praktiline_yl-master/src/SeleniumConfig.java
@@ -1,48 +1,55 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.JavascriptExecutor;
+import static org.testng.AssertJUnit.assertEquals;
 
-public class SeleniumConfig {
+public class CalculatorTest {
     public static void main(String[] args) throws InterruptedException {
-        //Chrome driver
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Karl\\Downloads\\chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver", "/Users/Karl/Downloads/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        //külasta LHV liisingu lehekülge
-        driver.get("https://www.lhv.ee/liising");
-
-        // täismõõdus brauseriaken
+        driver.get("https://www.lhv.ee/et/liising#kalkulaator");
         driver.manage().window().maximize();
-
-        //oota kuni leht avaneb
-        Thread.sleep(2000);
 
         //accept cookies
         driver.findElement(By.id("acceptPirukas")).click();
 
         //teeme kindlaks, et oleme Liisingu vahelehel
-        String title = driver.getTitle();
-        System.out.println(title);
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Liising · LHV";
+        assertEquals(expectedTitle,actualTitle);
 
-        // Soovin liisingut "juriidilise isikuna" - (eraisik on by default juba valitud)
-        driver.findElement(By.xpath("//*[contains(text(), 'juriidilise isikuna')]")).click();
-
-        // Liisingu tüüp "kasutusrent" - (kapitalirent on by default juba valitud)
-        driver.findElement(By.xpath("//*[contains(text(), 'kasutusrent')]")).click();
+        //Kerime kalkukaatori akna juurde
+        String linkText = "Näidiskuumakse";
+        driver.findElement(By.linkText(linkText));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        String script = "arguments[0].scrollIntoView(true);";
+        jsExecutor.executeScript(script, driver.findElement(By.linkText(linkText)));
 
         //Sõiduki uue hinna määramine
         WebElement autohind = driver.findElement(By.id("price"));
         autohind.clear();
-        autohind.sendKeys("1000");
-
-        // "Hind sisaldab käibemaksu" linnukese kastist eemaldamine (by default on linnuke olemas)
-        driver.findElement(By.xpath("//*[contains(text(), 'Hind sisaldab')]")).click();
+        autohind.sendKeys("20000");
 
         //"Sissemakse %" textfieldi muutmine
         WebElement sissemaks = driver.findElement(By.id("initial_percentage"));
         sissemaks.clear();
         sissemaks.sendKeys("15");
+        // Sissemakse eurodes kontroll - (sõiduki hind * sissemakse %) / 100 = sissemakse €
+        WebElement paymentEuros = driver.findElement(By.id("initial"));
+        String presentedNumberTxt = paymentEuros.getAttribute("value");
+        int presentedNumberInt = Integer.parseInt(presentedNumberTxt);
+        int expectedNumber = 3000;
+        if (presentedNumberInt == expectedNumber){
+            System.out.println("sissemaks eurodes on arvutatud korrektselt: " + presentedNumberInt + "€" );
+        } else {
+            System.out.println("sissemaks on vale, õige arv €: " + expectedNumber + ", kuvatud arv €: " + presentedNumberTxt );
+        }
 
         //liisingu periood - valime liisinguperioodiks 2 aastat ja 6 kuud
         Select liisingAasta = new Select(driver.findElement(By.name("years")));
@@ -60,13 +67,36 @@ public class SeleniumConfig {
         jaakvaartus.clear();
         jaakvaartus.sendKeys("20");
 
+        //jääkväärtuse summa kontroll
+        WebElement reminder = driver.findElement(By.id("reminder"));
+        String presentedReminder = reminder.getAttribute("value");
+        int presentedReminderInt = Integer.parseInt(presentedReminder);
+        int expectedReminder = 4000;
+        if (presentedReminderInt == expectedReminder){
+            System.out.println("Jääkväärtuse summa eurodes on: " + presentedReminderInt);
+        } else {
+            System.out.println("Vigane jääkväärtuse summa");
+        }
+
+        TimeUnit.SECONDS.sleep(2);
+
         //Maksegraafiku avamine
-        driver.findElement(By.cssSelector(".arrow.payment-graph-link")).click();
+        WebElement maksegraafik = driver.findElement(By.cssSelector("a.arrow.payment-graph-link.margin-top-20"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", maksegraafik);
+        //Kontrollime, kas maksegraafik avaneb
+        WebElement maksegraafikOpen = driver.findElement(By.className("text-center"));
+        if (maksegraafikOpen.isDisplayed()) {
+            System.out.println("Oled maksegraafiku lehel");
+        } else {
+            System.out.println("Maksegraafiku lehte ei õnnestunud avada");
+        }
 
-        // oota 5 sek
-        Thread.sleep(5000);
+        //table rows
+        List<WebElement> links = driver.findElements(By.cssSelector("tr"));
+        System.out.println(links.size());
 
-        //sulgeb kogu testrakenduse
-        driver.quit();
+        //Kogu testrakenduse sulgemine
+        //driver.quit();
     }
 }
